53eb4f509ee8f4933dd2f78f89c14e18713c6645
theta <willosh1129@gmail.com>
2026-08-27 08:23:35 +0900

    a

--- Assets/Scripts/will/EnemySpawner.cs
+++ Assets/Scripts/will/EnemySpawner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,6 +8,9 @@ public class EnemySpawner : MonoBehaviour
     private float summonCooldown = 0f;
 
     [SerializeField] private GameObject enemyPrefab;
+
+    private List<GameObject> enemies = new();
+    [SerializeField] private int maxEnemyCount = 5;
     void Start()
     {
         
@@ -20,15 +24,29 @@ public class EnemySpawner : MonoBehaviour
             if (summonCooldown <= 0)
             {
                 Summon();
-                summonCooldown = UnityEngine.Random.Range(5, 8);
+                summonCooldown = UnityEngine.Random.Range(3, 6);
             }
         }
     }
 
     private void Summon()
     {
+        List<GameObject> tmp = new();
+        foreach (var e in enemies)
+        {
+            if (e != null)
+            {
+                tmp.Add(e);
+            }
+        }
+        enemies = tmp;
+        if (enemies.Count > maxEnemyCount)
+        {
+            return;
+        }
         var enemy = Instantiate(enemyPrefab);
         float angle = UnityEngine.Random.Range(0, 360);
         enemy.transform.position = Player.Instance.transform.position + (15 * new Vector3((float)Math.Cos(angle), (float)Math.Sin(angle), 0));
+        enemies.Add(enemy);
     }
 }
